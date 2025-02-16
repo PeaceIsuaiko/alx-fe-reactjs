95759f4214c3cd6386cca2cc11fd584758628da6
PeaceIsuaiko <peaceisuaiko2000@gmail.com>
2025-02-17 00:00:00 +0100

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     <Header />
     <MainContent />
     <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
-    <Footer/>
+    <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
