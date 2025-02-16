954a509deb7f52c9515ebc02243edb775cd12002
PeaceIsuaiko <peaceisuaiko2000@gmail.com>
2025-02-16 23:47:51 +0100

    Update App.jsx
    Added UserProfile component and updated App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from './components/welcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -15,7 +16,8 @@ function App() {
     <WelcomeMessage />
     <Header />
     <MainContent />
-    <Footer />
+    <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
+    <Footer/>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
