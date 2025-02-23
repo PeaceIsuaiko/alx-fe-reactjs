30db2ecd6caee7e3fa78ff9c7c14f1f36360ce22
PeaceIsuaiko <peaceisuaiko2000@gmail.com>
2025-02-23 22:53:31 +0100

    Update Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,15 +1,35 @@
+
+import { Link } from "react-router-dom"; 
+
 function Navbar() {
-  return (
+  return (n (
     <nav style={{ backgroundColor: "#333", display: "flex", justifyContent: "space-between", padding: "10px" }}>
       <h1>My Company</h1>
       <ul style={{ display: "flex", gap: "15px", listStyle: "none" }}>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/services">Services</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li><Link to="/" style={linkStyle}>Home</Link></li>
+        <li><Link to="/about" style={linkStyle}>About</Link></li>
+        <li><Link to="/services" style={linkStyle}>Services</Link></li>
+        <li><Link to="/contact" style={linkStyle}>Contact</Link></li>
       </ul>
     </nav>
   );
 }
 
 export default Navbar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
