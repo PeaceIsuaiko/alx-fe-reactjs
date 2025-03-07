1864b3d0e7e6a212e1f88e63268d2edc73a55c9b
Peace Stephen Isuaiko <peaceisuaiko2000@gmail.com>
2025-03-07 16:28:32 +0100

    eerors messaage added

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -7,7 +7,22 @@ const RegistrationForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    let validationErrors = {};
+
+
+    if(!username) validationErrors.username = "Username is required";
+    if(!email) validationErrors.email = "Email is required";
+    if(!password) validationErrors.password = "Password is required";
+
+
+
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
+
     console.log({ username, email, password });
+    setErrors({});
   };
 
   return (
@@ -20,6 +35,7 @@ const RegistrationForm = () => {
           onChange={(e) => setUsername(e.target.value)}
           required
         />
+        {errors.username && <p style={{color: "red"}}>{errors.username}</p> }
       </label>
       <br />
       <label>
@@ -30,6 +46,7 @@ const RegistrationForm = () => {
           onChange={(e) => setEmail(e.target.value)}
           required
         />
+        {errors.email && <p style={{color: "red"}}>{errors.email}</p> }
       </label>
       <br />
       <label>
@@ -40,6 +57,7 @@ const RegistrationForm = () => {
           onChange={(e) => setPassword(e.target.value)}
           required
         />
+        {errors.password && <p style={{color: "red"}}>{errors.password}</p> }
       </label>
       <br />
       <button type="submit">Register</button>
