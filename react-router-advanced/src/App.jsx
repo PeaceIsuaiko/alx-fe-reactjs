4059be83cf4c8886c3bcb5c29780d1e541239258
Peace Stephen Isuaiko <peaceisuaiko2000@gmail.com>
2025-03-08 13:39:51 +0100

    dynamic routing added

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,16 +1,23 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Profile from "./components/Profile";
+import About from "./components/About";
+import BlogPost from "./components/BlogPost";
+
 
 function App() {
   return (
     <Router>
       <nav>
         <Link to="/">Home</Link> | 
+        <Link to="/about">About</Link>
         <Link to="/profile">Profile</Link>
+        
       </nav>
 
       <Routes>
+      <Route path="/about" element={<About />} />
         <Route path="/profile/*" element={<Profile />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
   );
