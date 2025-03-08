524e575895cc3b03ffdc9892a3eb3b35a556e666
Peace Stephen Isuaiko <peaceisuaiko2000@gmail.com>
2025-03-08 14:17:03 +0100

    Authentication added

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,21 +2,34 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Profile from "./components/Profile";
 import About from "./components/About";
 import BlogPost from "./components/BlogPost";
-
+import Login from "./components/Login"
+import ProtectedRoute from "./components/ProtectedRoute"; 
+import { useState } from "react";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false); 
+
   return (
     <Router>
       <nav>
         <Link to="/">Home</Link> | 
-        <Link to="/about">About</Link>
+        <Link to="/about">About</Link> | 
         <Link to="/profile">Profile</Link>
-        
       </nav>
 
       <Routes>
-      <Route path="/about" element={<About />} />
-        <Route path="/profile/*" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
+        <Route 
+          path="/profile/*" 
+          element={
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
+              <Profile />
+            </ProtectedRoute>
+          } 
+        />
+        
+
         <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
