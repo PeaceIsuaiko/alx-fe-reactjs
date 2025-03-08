6678ae45c3d3671c5e5343713a90b3df9d29cb67
Peace Stephen Isuaiko <peaceisuaiko2000@gmail.com>
2025-03-08 13:02:29 +0100

    corrected

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,11 +1,13 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
 const Profile = () => {
   return (
     <div>
-      <h1>Profile Page</h1>
-   l   <Link to="/profile/settings">Go to Settings</Link>
+      <h2>My Profile</h2>
+      <p>Name: Peace Stephen Isuaiko</p>
+      <p>Email: peaceisuaiko2000@gmail.com</p>
+      <Link to="/profile/details">View Details</Link> | 
+      <Link to="/profile/settings">Settings</Link>
     </div>
   );
 };
