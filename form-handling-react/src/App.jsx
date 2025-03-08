4104db74e5e5fbe6108fca87e715a37f87afbd9a
Peace Stephen Isuaiko <peaceisuaiko2000@gmail.com>
2025-03-08 09:55:26 +0100

    Added React Query demo project

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,11 +1,18 @@
+import React from "react";
 import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/formikForm";
 
 function App() {
   return (
     <div>
       <h1>Advanced Form Handling</h1>
       <RegistrationForm />
-    </div>
+    
+
+    
+    <h1>My App</h1>
+    <FormikForm />
+  </div>
   );
 }
 
