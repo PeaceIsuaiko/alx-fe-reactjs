2a27c7195a5757452dd6b66bccb4ee7f6e8a0fd9
Peace Stephen Isuaiko <peaceisuaiko2000@gmail.com>
2025-03-10 02:29:32 +0100

    delete file content corrected

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,11 +1,13 @@
 import useRecipeStore from './recipeStore';
+import {useNavigate} from "react-router-dom"
 
 const DeleteRecipeButton = ({ recipeId, onDelete }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+  const navigate = useNavigate();
 
   const handleDelete = () => {
     deleteRecipe(recipeId);
-    if (onDelete) onDelete();
+    navigate('/');
   };
 
   return <button onClick={handleDelete}>Delete Recipe</button>;
