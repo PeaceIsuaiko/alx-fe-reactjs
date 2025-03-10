351ffdbc00fe9f3c22f9f0991ee91c0c8d9551ea
Peace Stephen Isuaiko <peaceisuaiko2000@gmail.com>
2025-03-10 14:36:20 +0100

    favorites and recommendation functions added to recipe store content

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -5,7 +5,7 @@ const useRecipeStore = create((set) => ({
   searchTerm: "",
   filteredRecipes: [], 
 
-  // ✅ Function to update searchTerm and filter recipes
+  //  Function to update searchTerm and filter recipes
   setSearchTerm: (term) => set((state) => ({
     searchTerm: term,
     filteredRecipes: state.recipes.filter((recipe) =>
@@ -13,13 +13,26 @@ const useRecipeStore = create((set) => ({
     )
   })),
 
-  // ✅ Function to update recipes and refilter if necessary
+  // Function to update recipes and refilter if necessary
   setRecipes: (newRecipes) => set((state) => ({
     recipes: newRecipes,
     filteredRecipes: newRecipes.filter((recipe) =>
       recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
     )
   })),
+
+  //Favorites functionality
+  favorites: [],
+  addToFavorites: (recipe) =>
+    set((state) => ({ favorites: [...state.favorites, recipe] })),
+  removeFromFavorites: (id) =>
+    set((state) => ({
+      favorites: state.favorites.filter((recipe) => recipe.id !== id),
+    })),
+
+  // Recommendations functionality
+  recommendations: [],
+  setRecommendations: (recommendations) => set({ recommendations }),
 }));
 
 export default useRecipeStore;
