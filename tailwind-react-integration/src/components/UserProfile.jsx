6755b5bc4ac396e113aee0e0533fb860665a454b
Peace Stephen Isuaiko <peaceisuaiko2000@gmail.com>
2025-03-13 12:04:17 +0100

    correections

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,20 +1,14 @@
 function UserProfile() {
     return (
-<<<<<<< HEAD
-      <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-=======
-      <div className="bg-grey-100 p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-20 rounded-lg shadow-lg">
->>>>>>> Updated UserProfile component with responsive Tailwind styling
+      <div className="bg-grey-100 sm:p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-20 rounded-lg shadow-lg">
+
         <img src="https://via.placeholder.com/150"
         className="rounded-full w-24 h-24 sm:w-32 sm:h-32 mx-auto" 
         alt="User" />
-<<<<<<< HEAD
-        <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-        <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
-=======
+
         <h1 className="text-xl sm:text-xl md:text-2xl text-blue-800 my-3 sm:my-4">John Doe</h1>
         <p className="text-grey-600 text-sm sm:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
->>>>>>> Updated UserProfile component with responsive Tailwind styling
+
       </div>
     );
   }
