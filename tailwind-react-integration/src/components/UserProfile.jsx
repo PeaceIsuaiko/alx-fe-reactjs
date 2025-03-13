a959d4138e57425bd3094e2e21818f3dc1ad6ed6
PeaceIsuaiko <peaceisuaiko2000@gmail.com>
2025-03-13 12:35:45 +0100

    Update UserProfile.jsx
    Styled UserProfile component with Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,14 +1,11 @@
 function UserProfile() {
     return (
-      <div className="bg-grey-100 sm:p-4 sm:p-6 md:p-8  md:max-w-sm max-w-xs  mx-auto my-10 sm:my-20 rounded-lg shadow-lg">
-
+      <div className="hover:shadow-xl bg-gray-100 sm:p-4 p-6 md:p-8 md:max-w-sm max-w-xs mx-auto my-10 sm:my-20 rounded-lg shadow-lg">
         <img src="https://via.placeholder.com/150"
-        className="rounded-full mx-auto sm:h-24  sm:w-24  md:w-36 md:h-36" 
+        className="rounded-full hover:scale-110 mx-auto sm:h-24 sm:w-24 md:w-36 md:h-36 transition-transform duration-300 ease-in-out" 
         alt="User" />
-
-        <h1 className="text-xl   md:text-xl  md:text-2xl text-blue-800 my-3 sm:my-4">John Doe</h1>
-        <p className="text-grey-600 text-lg text-sm sm:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
-
+        <h1 className="text-xl md:text-2xl text-blue-800 my-3 sm:my-4 hover:text-blue-500">John Doe</h1>
+        <p className="text-gray-600 text-lg text-sm sm:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
     );
   }
