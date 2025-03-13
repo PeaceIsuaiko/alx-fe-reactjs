12c2b6305f86dc26e7f6450c8c064c4c47b24518
PeaceIsuaiko <peaceisuaiko2000@gmail.com>
2025-03-13 12:37:17 +0100

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,8 +4,8 @@ function UserProfile() {
         <img src="https://via.placeholder.com/150"
         className="rounded-full hover:scale-110 mx-auto sm:h-24 sm:w-24 md:w-36 md:h-36 transition-transform duration-300 ease-in-out" 
         alt="User" />
-        <h1 className="text-xl md:text-2xl text-blue-800 my-3 sm:my-4 hover:text-blue-500">John Doe</h1>
-        <p className="text-gray-600 text-lg text-sm sm:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+        <h1 className="md:text-xl md:text-2xl text-blue-800 my-3 sm:my-4 hover:text-blue-500">John Doe</h1>
+        <p className="text-gray-600 md:text-xl text-lg text-sm sm:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
     );
   }
