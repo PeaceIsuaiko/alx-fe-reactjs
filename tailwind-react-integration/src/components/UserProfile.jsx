8541b3c533e1cd14909158962f455cdd472154dd
Peace Stephen Isuaiko <peaceisuaiko2000@gmail.com>
2025-03-13 12:32:05 +0100

    Resolved merge conflicts

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,5 +1,9 @@
 function UserProfile() {
     return (
+<<<<<<< HEAD
+=======
+
+>>>>>>> Resolved merge conflicts
       <div className="hover:shadow-xl bg-gray-100 sm:p-4 p-6 md:p-8 md:max-w-sm max-w-xs mx-auto my-10 sm:my-20 rounded-lg shadow-lg">
 
         <img src="https://via.placeholder.com/150"
@@ -8,6 +12,10 @@ function UserProfile() {
 
         <h1 className="text-xl md:text-2xl text-blue-800 my-3 sm:my-4 hover:text-blue-500">John Doe</h1>
         <p className="text-gray-600 text-lg text-sm sm:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+<<<<<<< HEAD
+=======
+
+>>>>>>> Resolved merge conflicts
 
       </div>
     );
